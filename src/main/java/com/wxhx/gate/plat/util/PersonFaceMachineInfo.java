92dc378a191d99efbb089c64b912e4af4e67941f
叶开 <geyueliang_89@163.com>
2019-06-02 21:09:36 +0800

    人员信息

---
+++ src/main/java/com/wxhx/gate/plat/util/PersonFaceMachineInfo.java
@@ -0,0 +1,16 @@
+package com.wxhx.gate.plat.util;
+/**
+ * 人脸机器常用属性
+ * @author geliang
+ *
+ */
+public class PersonFaceMachineInfo {
+
+	//开发者id
+	public static String APPID = "mj4bfdecbef41e2faf";
+	
+	//设备id
+	public static String DEVICENO = "";
+	
+	
+}
---
+++ src/main/java/com/wxhx/gate/plat/util/PersonFacePlatUtil.java
@@ -0,0 +1,58 @@
+package com.wxhx.gate.plat.util;
+/**
+ * 人脸平台相关工具类
+ * @author geliang
+ *
+ */
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import com.alibaba.fastjson.JSONObject;
+import com.wxhx.basic_client.common.HXCoreUtil;
+
+@Component
+public class PersonFacePlatUtil {
+	
+	@Value("${wxhx.person.face.AppSecret:2bfcf060140643b0bee193b547ee83b0}")
+	private String appSecret;
+	
+	/**
+	 * 计算签名算法
+	 * @param <T>
+	 * @param t
+	 * @return
+	 */
+	public <T> String getSign(T t) {
+		//将对象转换成map
+		Map<String, Object> map = JSONObject.parseObject(HXCoreUtil.getJsonString(t), Map.class);
+		List<String> keys = new ArrayList<String>(map.keySet());
+		//排序
+		Collections.sort(keys);
+		//创建加密初始值
+		StringBuffer stringA = new StringBuffer();
+		for(String key:keys) {
+			stringA.append(key+"="+map.get(keys)+"&");
+		}
+		stringA.append(appSecret);
+		String result = HXCoreUtil.md5(stringA.toString());
+		return result.toUpperCase();
+	}
+	
+	/**
+	 *  获取时间戳
+	 * @return
+	 */
+	public String  getTimeTemp() {
+		String timeFormatStr = "yyyyMMddHHmmssfff";
+		SimpleDateFormat dateFormat = new SimpleDateFormat(timeFormatStr);
+		return dateFormat.format(new Date());
+	}
+}
