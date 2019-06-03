a0ca333fc5749bc5e7f90191f1fd671addf738c8
叶开 <geyueliang_89@163.com>
2019-06-03 22:18:06 +0800

    修改报道接口

--- src/main/java/com/wxhx/gate/plat/controller/RegisterController.java
+++ src/main/java/com/wxhx/gate/plat/controller/RegisterController.java
@@ -1,6 +1,7 @@
 package com.wxhx.gate.plat.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,14 +20,22 @@ import com.wxhx.gate.plat.service.IRegisterService;
 @RequestMapping("/register")
 public class RegisterController {
 	
+	@Value("${wxhx.kcxi.ddxx.ksdd:520300208}")
+	private String ksdd;
+	
 	@Autowired
 	private IRegisterService iRegisterService;
 	/**
-	 * 报道
+	    *报道
+	 *  @param sfzhm 身份证号码
 	 * @return
 	 */
 	@RequestMapping(method = RequestMethod.POST)
-	HXRespons<RegisterResponse> register(RegisterInfoVo registerInfoVo){
+	HXRespons<RegisterResponse> register(String sfzhm){
+		RegisterInfoVo registerInfoVo = new RegisterInfoVo();
+		registerInfoVo.setSfzmhm(sfzhm);
+		registerInfoVo.setKskm("2");
+		registerInfoVo.setKsdd(ksdd);
 		return iRegisterService.register(registerInfoVo);
 	}
 	
