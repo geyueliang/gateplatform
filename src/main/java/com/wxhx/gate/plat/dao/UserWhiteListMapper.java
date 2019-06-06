328cc13ac6458d71570cfe0ba892cbe772c5ff0e
叶开 <geyueliang_89@163.com>
2019-06-06 11:21:39 +0800

    新增管理人員白名單

---
+++ src/main/java/com/wxhx/gate/plat/dao/UserWhiteListMapper.java
@@ -0,0 +1,9 @@
+package com.wxhx.gate.plat.dao;
+
+import com.wxhx.gate.plat.dao.entity.UserWhiteList;
+
+import tk.mybatis.mapper.common.Mapper;
+
+public interface UserWhiteListMapper extends Mapper<UserWhiteList>{
+	
+}
