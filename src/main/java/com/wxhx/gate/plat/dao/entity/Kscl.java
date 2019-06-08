8d847a92dfc8a63c5ca097b369e7722b22dec4d9
佴广炜 <gaungwei.nai@wuxihuaxun.club>
2019-06-08 14:48:25 +0800

    bug修正

---
+++ src/main/java/com/wxhx/gate/plat/dao/entity/Kscl.java
@@ -0,0 +1,54 @@
+package com.wxhx.gate.plat.dao.entity;
+
+public class Kscl {
+	private String kcbh;
+	private String kchp;
+	private String kczt;
+	private String sptd;
+	private String kscx;
+	private String kcip;
+	private String lxxh;
+	public String getKcbh() {
+		return kcbh;
+	}
+	public void setKcbh(String kcbh) {
+		this.kcbh = kcbh;
+	}
+	public String getKchp() {
+		return kchp;
+	}
+	public void setKchp(String kchp) {
+		this.kchp = kchp;
+	}
+	public String getKczt() {
+		return kczt;
+	}
+	public void setKczt(String kczt) {
+		this.kczt = kczt;
+	}
+	public String getSptd() {
+		return sptd;
+	}
+	public void setSptd(String sptd) {
+		this.sptd = sptd;
+	}
+	public String getKscx() {
+		return kscx;
+	}
+	public void setKscx(String kscx) {
+		this.kscx = kscx;
+	}
+	public String getKcip() {
+		return kcip;
+	}
+	public void setKcip(String kcip) {
+		this.kcip = kcip;
+	}
+	public String getLxxh() {
+		return lxxh;
+	}
+	public void setLxxh(String lxxh) {
+		this.lxxh = lxxh;
+	}
+	
+}
