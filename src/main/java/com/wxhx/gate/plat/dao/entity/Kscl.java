0a66439bcadc40e3a95b511c0e01ac3a03038d01
叶开 <geyueliang_89@163.com>
2019-06-10 15:49:18 +0800

    初始化儿可预约车辆

--- src/main/java/com/wxhx/gate/plat/dao/entity/Kscl.java
+++ src/main/java/com/wxhx/gate/plat/dao/entity/Kscl.java
@@ -1,54 +1,73 @@
 package com.wxhx.gate.plat.dao.entity;
 
 public class Kscl {
-	private String kcbh;
-	private String kchp;
-	private String kczt;
-	private String sptd;
-	private String kscx;
-	private String kcip;
-	private String lxxh;
-	public String getKcbh() {
-		return kcbh;
-	}
-	public void setKcbh(String kcbh) {
-		this.kcbh = kcbh;
-	}
-	public String getKchp() {
-		return kchp;
-	}
-	public void setKchp(String kchp) {
-		this.kchp = kchp;
-	}
-	public String getKczt() {
-		return kczt;
-	}
-	public void setKczt(String kczt) {
-		this.kczt = kczt;
-	}
-	public String getSptd() {
-		return sptd;
-	}
-	public void setSptd(String sptd) {
-		this.sptd = sptd;
-	}
-	public String getKscx() {
-		return kscx;
-	}
-	public void setKscx(String kscx) {
-		this.kscx = kscx;
-	}
-	public String getKcip() {
-		return kcip;
-	}
-	public void setKcip(String kcip) {
-		this.kcip = kcip;
-	}
-	public String getLxxh() {
-		return lxxh;
-	}
-	public void setLxxh(String lxxh) {
-		this.lxxh = lxxh;
-	}
-	
+    private String kcbh;
+
+    private String kchp;
+
+    private int kczt;
+
+    private Short sptd;
+
+    private String kscx;
+
+    private String kcip;
+
+    private String lxxh;
+
+    public String getKcbh() {
+        return kcbh;
+    }
+
+    public void setKcbh(String kcbh) {
+        this.kcbh = kcbh == null ? null : kcbh.trim();
+    }
+
+    public String getKchp() {
+        return kchp;
+    }
+
+    public void setKchp(String kchp) {
+        this.kchp = kchp == null ? null : kchp.trim();
+    }
+
+    public int getKczt() {
+        return kczt;
+    }
+
+    public void setKczt(int kczt) {
+        this.kczt = kczt;
+    }
+
+    public Short getSptd() {
+        return sptd;
+    }
+
+    public void setSptd(Short sptd) {
+        this.sptd = sptd;
+    }
+
+    public String getKscx() {
+        return kscx;
+    }
+
+    public void setKscx(String kscx) {
+        this.kscx = kscx == null ? null : kscx.trim();
+    }
+
+    public String getKcip() {
+        return kcip;
+    }
+
+    public void setKcip(String kcip) {
+        this.kcip = kcip == null ? null : kcip.trim();
+    }
+
+    public String getLxxh() {
+        return lxxh;
+    }
+
+    public void setLxxh(String lxxh) {
+        this.lxxh = lxxh == null ? null : lxxh.trim();
+    }
 }
