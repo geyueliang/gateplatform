c58aea5293357762a903fd362ba0bf1cd459d100
叶开 <geyueliang_89@163.com>
2019-07-13 15:04:25 +0800

    修改日志打印

--- src/main/java/com/wxhx/gate/plat/service/impl/ExamProcessServiceImpl.java
+++ src/main/java/com/wxhx/gate/plat/service/impl/ExamProcessServiceImpl.java
@@ -73,11 +73,12 @@ public class ExamProcessServiceImpl implements IExamProcessService{
 	public String idCheck(IdentityComparison comparison) throws Exception {
 		String writeXml = HXCallWebServiceUtil.beanToXml(comparison);
 		String jkid = "17C51"; //身份比對
-//		HXLogUtil.info(logger,"身份对比调用{0},入参{1}",jkid,HXCallWebServiceUtil.enCodeStr(writeXml));
+		HXLogUtil.info(logger,"身份对比调用{0},入参{1}",jkid,HXCallWebServiceUtil.enCodeStr(writeXml));
 		String result = HXCallWebServiceUtil.writeWebService(jkid, writeXml);
 		//验证成功开始第一个项目
 		ItemBegin itemBegin = this.getItemBegin(comparison.getSfzmhm(), null, comparison);
 		this.itemBegin(itemBegin);
+		HXLogUtil.info(logger,"身份对比返回结果{0}",result);
 		return result;
 	}
 
@@ -89,8 +90,10 @@ public class ExamProcessServiceImpl implements IExamProcessService{
 	public String itemBegin(ItemBegin itemBegin) throws Exception {
 		String writeXml = HXCallWebServiceUtil.beanToXml(itemBegin);
 		String jkid = "17C52";	//项目开始接口
-//		HXLogUtil.info(logger,"项目开始调用{0},入参{1}",jkid,writeXml);
-		return HXCallWebServiceUtil.writeWebService(jkid, writeXml);
+		HXLogUtil.info(logger,"项目开始调用{0},入参{1}",jkid,writeXml);
+		String result =  HXCallWebServiceUtil.writeWebService(jkid, writeXml);
+		HXLogUtil.info(logger,"项目调用返回{0}",result);
+		return result;
 	}
 	
 	/**
@@ -100,7 +103,7 @@ public class ExamProcessServiceImpl implements IExamProcessService{
 	public String examMarkHappen(ExamMark examMark,ExamItemEnd examItemEnd) throws Exception {
 		String writeXml = HXCallWebServiceUtil.beanToXml(examMark);
 		String jkid = "17C53"; //考试扣分
-//		HXLogUtil.info(logger,"发生扣分调用{0},入参{1}",jkid,writeXml);
+		HXLogUtil.info(logger,"发生扣分调用{0},入参{1}",jkid,writeXml);
 		//判断当前的分数加上已经扣的分数 是否达到结束考试要求
 		int nowKf = InitKSKFDM.getKf(examMark.getKfxm());
 		//获取当前已经扣除分数
@@ -109,6 +112,8 @@ public class ExamProcessServiceImpl implements IExamProcessService{
 		 *  超过20分 考试不及格 调用项目结束和 科目结束进行处理
 		 */
 		if((nowKf+currentSum)>20) {
+			HXLogUtil.info(logger,"=============扣分大于20分执行 结束逻辑============");
+
 			//复制基本属性
 			BeanUtils.copyProperties(examItemEnd, examMark);
 			ExamEnd examEnd = this.createExamEnd(examItemEnd,(nowKf+currentSum));
@@ -122,8 +127,10 @@ public class ExamProcessServiceImpl implements IExamProcessService{
 	public String uploadImage(ProcessImage processImage) throws Exception {
 		String writeXml = HXCallWebServiceUtil.beanToXml(processImage);
 		String jkid = "17C54"; //图片上传
-//		HXLogUtil.info(logger,"上传图片调用{0},入参{1}",jkid,writeXml);
-		return HXCallWebServiceUtil.writeWebService(jkid, writeXml);
+		HXLogUtil.info(logger,"上传图片调用{0},入参{1}",jkid,writeXml);
+		String result =  HXCallWebServiceUtil.writeWebService(jkid, writeXml);
+		HXLogUtil.info(logger,"上传图片调用返回结果",result);
+		return result;
 	}
 
 	/**
@@ -134,6 +141,7 @@ public class ExamProcessServiceImpl implements IExamProcessService{
 		String result = "";
 		//是最后的项目 调用科目考试结束
 		if(HXCoreUtil.isEquals("20500", examItemEnd.getKsxm())) {
+			HXLogUtil.info(logger,"最后一个项目结束 直接科目结束");
 			//调用科目考试结束
 			ExamEnd examEnd = this.createExamEnd(examItemEnd,0);
 //			HXLogUtil.info(logger,"开始科目结束,项目入参{0},科目入参{1}",examItemEnd,examEnd);
@@ -142,17 +150,21 @@ public class ExamProcessServiceImpl implements IExamProcessService{
 		else {
 			String writeXml = HXCallWebServiceUtil.beanToXml(examItemEnd);
 			String jkid = "17C55"; //项目结束
-//			HXLogUtil.info(logger,"项目结束调用{0},入参{1}",jkid,writeXml);
+			HXLogUtil.info(logger,"项目结束调用{0},入参{1}",jkid,writeXml);
 			result =  HXCallWebServiceUtil.writeWebService(jkid, writeXml);
 			//判断当前扣分是否超过20 	超过20分 结束考试
 			int kfhj = this.getKskf(examItemEnd.getSfzmhm());
 			if(kfhj>20) {
+				HXLogUtil.info(logger,"项目结束后分数超过 20 分 科目结束==============");
 				ExamEnd examEnd = this.createExamEnd(examItemEnd,kfhj);
 				result = this.examEnd(examEnd);
+				HXLogUtil.info(logger,"项目结束中进行科目结束 结果{0}",result);
 				return result;
 			}
+			HXLogUtil.info(logger,"项目结束结果",result);
 		}
 		//项目结束调用下一个项目的开始
+		HXLogUtil.info(logger,"*********************下一个项目开始调用*********************");
 		ItemBegin itemBegin = this.getItemBegin(examItemEnd.getSfzmhm(), examItemEnd, null);
 		if(itemBegin!=null) {
 			this.itemBegin(itemBegin);
@@ -164,15 +176,17 @@ public class ExamProcessServiceImpl implements IExamProcessService{
 	public String examEnd(ExamEnd examEnd) throws Exception {
 		String writeXml = HXCallWebServiceUtil.beanToXml(examEnd);
 		String jkid = "17C56"; //科目结束
-//		HXLogUtil.info(logger,"科目结束调用{0},入参{1}",jkid,writeXml);
-		return HXCallWebServiceUtil.writeWebService(jkid, writeXml);
+		HXLogUtil.info(logger,"科目结束调用{0},入参{1}",jkid,writeXml);
+		String result = HXCallWebServiceUtil.writeWebService(jkid, writeXml);
+		HXLogUtil.info(logger,"科目结束调用返回{0}",result);
+		return result;
 	}
 	
 	@ExamProcessLogSaveAnnotation
 	public String writeVideo(WirteVideo wirteVideo) throws Exception {
 		String writeXml = HXCallWebServiceUtil.beanToXml(wirteVideo);
 		String jkid = "17E14"; //视频认证发启（写入）
-//		HXLogUtil.info(logger,"发起视频认证调用{0},入参{1}",jkid,writeXml);
+		HXLogUtil.info(logger,"发起视频认证调用{0},入参{1}",jkid,writeXml);
 		return HXCallWebServiceUtil.writeWebService(jkid, writeXml);
 	} 
 	
@@ -180,7 +194,7 @@ public class ExamProcessServiceImpl implements IExamProcessService{
 	public String readVideo(ReadVideo readVideo) throws Exception {
 		String writeXml = HXCallWebServiceUtil.beanToXml(readVideo);
 		String jkid = "17E15"; //读取视频认证结果
-//		HXLogUtil.info(logger,"读取视频认证结果调用{0},入参{1}",jkid,writeXml);
+		HXLogUtil.info(logger,"读取视频认证结果调用{0},入参{1}",jkid,writeXml);
 		return HXCallWebServiceUtil.queryWebService(jkid, writeXml);
 	} 
 
@@ -236,34 +250,34 @@ public class ExamProcessServiceImpl implements IExamProcessService{
 			examItemEnd.setKsxm(processArray[6]); //考试项目
 			//开始调用扣分
 			result = this.examMarkHappen(examMark,examItemEnd);
-			HXLogUtil.info(logger,"考试扣分返回{0}",result);
+			HXLogUtil.debug(logger,"考试扣分返回{0}",result);
 			break;
 		//图片上传
 		case 2:
 			result = this.uploadImage((ProcessImage) getCallBeanFromArray(processArray,typeId));
-			HXLogUtil.info(logger,"图片上传返回{0}",result);
+			HXLogUtil.debug(logger,"图片上传返回{0}",result);
 			break;
 		//项目结束
 		case 3:
 			result = this.examItemEnd((ExamItemEnd) getCallBeanFromArray(processArray,typeId));
-			HXLogUtil.info(logger,"项目结束返回{0}",result);
+			HXLogUtil.debug(logger,"项目结束返回{0}",result);
 			break;
 		//视频认证发启（写入）	
 		case 5:
 			result = this.writeVideo((WirteVideo) getCallBeanFromArray(processArray,typeId));
-			HXLogUtil.info(logger,"视频认证发启{0}",result);
+			HXLogUtil.debug(logger,"视频认证发启{0}",result);
 			break;
 		//读取视频认证结果
 		case 6:
 			result = this.readVideo((ReadVideo) getCallBeanFromArray(processArray,typeId));
-			HXLogUtil.info(logger,"读取视频认证结果{0}",result);
+			HXLogUtil.debug(logger,"读取视频认证结果{0}",result);
 			break;
 		default:
 			break;
 		}
 		//解析返回对象
 		WebServiceResult serviceResult =  HXCallWebServiceUtil.xmlToBean(result, WebServiceResult.class);
-		HXLogUtil.info(logger,"process处理 typeId{0}返回的json{1}",typeId,serviceResult);
+		HXLogUtil.debug(logger,"process处理 typeId{0}返回的json{1}",typeId,serviceResult);
 		if(serviceResult!=null&&serviceResult.getHead()!=null) {
 			//视频认证结果返回
 			WebServiceResultHead head = serviceResult.getHead();
